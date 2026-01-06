fbbbeb181ba7daff6af8c66ecdf3fbfde7153a02
satyam gautam <gautamsatyam47@gmail.com>
2026-01-06 17:00:15 +0530

    fixed cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
  
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
